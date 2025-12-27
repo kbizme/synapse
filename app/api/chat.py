cfc111fb35f57d65bf12c3499b5e272953fa3c59
kbizme <devs.kbizme@gmail.com>
2025-12-27 23:56:31 +0530

    UX & UI Updates...

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from app.agents import agents
 from app.schemas.chat import ChatRequest, ChatResponse, ChatResetRequest
 from app.core.memory import ChatManager
@@ -22,13 +22,20 @@ def chat(request: ChatRequest):
 def get_chat(chat_id: str):
     with get_session() as session:
         assert session is not None
-        chat = MessageRepository.get_messages_by_chat_id(db_session=session, chat_id=chat_id)
-    
-    return {"status": "ok", "chat_id": chat_id, "messages": [
+        chat = ChatRepository.get_by_id(db_session=session, chat_id=chat_id)
+        if chat is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Chat not found"
+            )
+        chat_messages = MessageRepository.get_messages_by_chat_id(db_session=session, chat_id=chat_id)
+        results = {"status": "ok", "id": chat_id, 'chat_title': chat.title, "messages": [
                                                             {"role": message.role, 
                                                              "content": message.content
-                                                            } for message in chat]
-            }
+                                                            } for message in chat_messages
+                                                        ]
+                    }
+    return results
     
 
 @router.post("/reset")
@@ -42,9 +49,9 @@ def all_chats():
     with get_session() as session:
         assert session is not None
         chats =  ChatRepository.get_all(db_session=session)
+        results = {"status": "ok", "chats": [ 
+                                    {"id": chat.id, "title": chat.title, "updated_at": chat.updated_at}
+                                    for chat in chats]
+                                    }
 
-    return {"status": "ok", "chats": [ 
-                                    {"id": chat.id, "title": chat.title, "updated_at": chat.updated_at} 
-                                    for chat in chats
-                                    ]
-            }
+    return results
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,7 +1,28 @@
 const input = document.getElementById('user-input');
-const container = document.getElementById('messages-container');
+const messagesContainer = document.getElementById('messages-container');
 const welcomeMessage = document.getElementById('welcome-message');
+const chatUl = document.getElementById('chat-list');
+const noChatsBanner = document.getElementById('no-chats-banner');
+
 let CURRENT_CHAT_ID;
+
+
+
+// page onload events and functions
+document.addEventListener('DOMContentLoaded', async () => {
+    // loading the chat history
+    await LoadChatLists();
+
+    // handling page load
+    const params = new URLSearchParams(window.location.search);
+    const chat_id = params.get('chat_id');
+    // handling current chat
+    if (chat_id) {
+        LoadCurrentChats(chat_id);
+    } else {
+        createNewChat();
+    }
+});
 
 
 
@@ -17,8 +38,8 @@ function sendMessage() {
     if (!text) return;
 
     // removing the intial greeting
-    if (container.children.length === 1) {
-        container.innerHTML = '';
+    if (messagesContainer.children.length === 1) {
+        messagesContainer.innerHTML = '';
     }
 
     // checking if current chat id is initialized
@@ -33,10 +54,10 @@ function sendMessage() {
     const userDiv = document.createElement('div');
     userDiv.className = 'message-bubble user-msg';
     userDiv.innerText = text;
-    container.appendChild(userDiv);
+    messagesContainer.appendChild(userDiv);
 
     input.value = '';
-    container.scrollTop = container.scrollHeight;
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
 
     // sending the api request
     fetch("http://localhost:8000/chat", {
@@ -50,10 +71,10 @@ function sendMessage() {
     .then(res => res.json())
     .then(data => {
         const aiDiv = document.createElement("div");
-        aiDiv.className = "message-bubble ai-msg";
-        aiDiv.innerText = data.reply;
-        container.appendChild(aiDiv);
-        container.scrollTop = container.scrollHeight;
+        aiDiv.className = "message-bubble assistant-msg";
+        aiDiv.innerHTML = marked.parse(data.reply, {breaks: true, gfm: true});
+        messagesContainer.appendChild(aiDiv);
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
     })
     .catch(err => {
         console.error(err);
@@ -64,23 +85,115 @@ function sendMessage() {
 
 
 function createNewChat() {
-    // if chat exists, then reseting the memory of that chat
-    if (CURRENT_CHAT_ID) {
-        fetch("http://localhost:8000/reset", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ chat_id: CURRENT_CHAT_ID })
-        }).catch(console.error);
-    }
-
+    // clearing active chat marker
+    ClearActiveChats();
     // initializing new chat id
     CURRENT_CHAT_ID = crypto.randomUUID();
+    // clearing URL 
+    history.pushState({}, "", "/");
 
-    container.innerHTML = `
+    // displaying welcome message
+    messagesContainer.innerHTML = `
         <div id="welcome-message">
-            <img src="static/images/synapse.png" alt="Synapse Logo"/>
+            <img src="/static/images/synapse.png" alt="Synapse Logo"/>
             <p>Hello, How can I help you today?</p>
         </div>
     `;
+
+    // changint the tab title
+    document.title = "Synapse";
+
+    // focuing on input
+    input.focus();
 }
 
+
+// handling previous chats and chat history
+
+async function LoadChatLists() {
+    const res = await fetch("http://localhost:8000/all-chats");
+    const data = await res.json();
+    RenderChatLists(data.chats);
+}
+
+function RenderChatLists(all_chats){
+    chatUl.innerHTML = '';
+    if (!all_chats || all_chats.length === 0){
+        console.log('no chats found');
+        noChatsBanner.style.display = 'block';
+        chatUl.style.display = 'none';
+        createNewChat();
+        return;
+    };
+
+    // otherwise, if previous chat found
+    noChatsBanner.style.display = 'none';
+    chatUl.style.display = 'block';
+    
+    all_chats.forEach(chat => {
+        const item = document.createElement('li')
+        item.textContent = chat.title;
+        item.classList.add('chat-item');
+        item.dataset.uuid = chat.id;
+        item.setAttribute('title', chat.title);
+        chatUl.appendChild(item);
+        item.addEventListener('click', (e) => {
+            ClearActiveChats();
+            e.currentTarget.classList.add('active');
+            // loading this clicked chat
+            LoadCurrentChats(chat.id);
+        });
+    });
+        
+}
+
+
+function ClearActiveChats(){
+    const activeChats = document.querySelectorAll('.chat-item');
+    activeChats.forEach(chat => {
+        chat.classList.remove('active');
+    });
+}
+
+
+
+function LoadCurrentChats(chat_id){
+    // updating the URL (no reload)
+    history.pushState(
+        { chat_id },
+        "",
+        `/?chat_id=${chat_id}`
+    );
+    // renaming the pate title
+    // fetching the current chat
+    fetch(`http://localhost:8000/chat/${chat_id}`)
+    .then(res => res.json())
+    .then(data => {
+        document.title = data.chat_title;
+        RenderCurrentChatMessages(data);
+    });
+}
+
+
+
+function RenderCurrentChatMessages(chat_data){
+    if (!chat_data || !chat_data.messages || chat_data.messages.length === 0) {
+        messagesContainer.innerHTML = "<p>No messages yet</p>";
+        return;
+    }
+
+    // updating the current chat id
+    CURRENT_CHAT_ID = chat_data.chat_id;
+    
+    // clearing the message container
+    messagesContainer.innerHTML = '';
+
+    // rendering the current chat messages inside the container
+    chat_data.messages.forEach(msg => {
+        const div = document.createElement('div');
+        div.className = msg.role === 'user' ? 'message-bubble user-msg' : 'message-bubble assistant-msg';
+        div.innerHTML = marked.parse(msg.content, {breaks: true, gfm: true});
+        messagesContainer.appendChild(div);
+    });
+
+}
